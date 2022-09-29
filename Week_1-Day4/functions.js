c2b15345d82f3820043673ed041741909ce38b39
Isaiah Benavides <benavidesisaiah@outlook.com>
2022-09-29 16:03:45 -0400

    Day 4 start commit

---
+++ Week_1-Day4/functions.js
@@ -0,0 +1,112 @@
+// functions
+
+// const x = 1
+// const y = 2
+// const z = 3
+// const w = 4
+//long and must be redone
+// (x+y+z+w) + 3;
+
+//this will hoist to the top!
+
+// function addition(x, y) {
+//     let sum = x + y
+//     console.log(sum)
+// }
+
+// addition(a(2 * 3, 5), a(16 / 2, 7))
+
+// addition(45, 9)
+
+// addition('I like ', 'Turtles!')
+
+// //Many Arguments
+// //this will not hoist to the top!
+// const spreadArgs = (...args) => {
+//     console.log(args)
+// }
+
+// spreadArgs(1, 2, 3, 4, "chesse", 79)
+
+// //using the return value the computer will log the 
+// function a(x, y) {
+//     let sum = x + y
+//     return sum
+// }
+
+// //Excersise
+
+// function addThreeNums(x, y, z) {
+//     return x + y + z
+
+// }
+
+// let sum1 = addThreeNums
+// console.log(sum1(a(3, 3), a(20, -5), a(8.5, 37 / 8)))
+
+
+// const addDivide = (a, b, c) => {
+//     return (a + b + c) / 3
+// }
+
+// console.log(addDivide(10, 20, 600))
+
+// const findAverage = (...args) => {
+//     let base = 0
+//     console.log(args)
+//     args.forEach((num) => {
+//         base += num
+//     })
+//     return base / args.length
+// }
+
+// console.log(findAverage(2,58,89,10,53))
+
+// let traker = 0
+
+// function printHelloWorld(str) {
+//     console.log(str)
+//     printName("- Toby")
+// }
+
+// function printName (name) {
+//     console.log(name)
+//     //console.trace("my first trace")
+// }
+
+// const increment = () => {
+//     const aVar = 'hello shvverlt'
+//     traker++
+//     console.log(traker)
+//     printHelloWorld(aVar)
+// }
+
+
+// // this shall bthrow an error as aVar is only defined inside the function!
+// // console.log(aVar)
+
+// //as the increment function is called it shall increment tracker by 1!
+// increment()
+// increment()
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 === 0 && i % 3 != 0) {
+        console.log("Buzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
+    } else {
+        console.log(i)
+    }
+}
+
+
+// for (let i = 10; i <= 100; i++) {
+//     if (i % 10 === 0 && i % 20 != 0) {
+//         console.log(i)
+//     }
+// }
+// for (let i = 10; i <=100; i++) {
+//     if(i % 20 === 0){
+//         console.log(i)
+//     }
+// }
--- Week_1-Day4/while-loops.js
+++ Week_1-Day4/while-loops.js
@@ -1,4 +1,155 @@
 //while loops!!!
+//i can use while loops when i don't know how long i need to loop for!
+//for loops are better suited to situations when I know how long i need the loop to go for!
+// let age = 0
+
+// while (age < 18) {
+//     console.log("Imma kid")
+//     console.log(`It's my birthday!! I'm ${age} years old.`) 
+//     age ++
+// }
+
+// let a = 1
+
+// while (a < 7) {
+//     if (a % 2 === 0) {
+//         console.log(`${a} is odd`)
+//     } else {
+//         console.log(`${a} is even`)
+//     }
+//     a++
+// }
+
+// let arr = [0, 1, 2]
+
+// while (arr.length > 1) {
+//     console.log(arr)
+//     arr.pop()
+// }
+
+
+// //excersise
+
+// //1
+// let b = 10
+
+// while (b >= 0) {
+//     console.log(b)
+//     b--;
+// }
+
+// //2
+
+// let c = 3
+
+// while (c < 35) {
+//     if (c % 3 === 0) {
+//         console.log(c + ' is a multiple of 3')
+//     } c++
+// }
+
+// //3
+
+// let d = 1
+
+// while (d < 100) {
+//     if (d % 5 === 0) {
+//         console.log(d + " is a multiple of 5")
+//     } d++
+// }
+
+// //4
+
+// let e = 1
+
+// while (e < 20) {
+//     if (e % 2 === 0) {
+//         console.log(e * 3)
+//     } e++
+// }
+
+//While loop labs
+
+//1
+// let studentGrade = 80
+
+// if (studentGrade < 55) {
+//     console.log("Bad Grade But You'll Get Em Next Time")
+// }
+// else if (studentGrade <= 55) {
+//     console.log("Not to good")
+// } else if (studentGrade <= 60) {
+//     console.log("ehhhh")
+// } else if (studentGrade <= 70) {
+//     console.log("Keep trying")
+// } else {
+//     console.log("You did a good job")
+// }
+
+
+// let grade = Math.floor(Math.random() * 100)
+
+// if (grade >= 80) {
+//     console.log("You did a good job")
+// } else if(grade >= 70){
+//     console.log("Keep trying")
+// } else if(grade >= 60){
+//     console.log("ehhhh")
+// } else (grade >= 55)
+//     console.log("Not to good")
+
+//2
+
+for (let i = 10; i <= 100; i++) {
+    if (i % 10 === 0 && i % 20 != 0) {
+        console.log(i)
+    }
+}
+for (let i = 10; i <=100; i++) {
+    if(i % 20 === 0){
+        console.log(i)
+    }
+}
+
+//3
+
+let i = 10
+while (i <= 100 ) {
+    if (i % 10 === 0 && i % 20 != 0) {
+        console.log(i)
+    }
+    i++
+}
+i = 10
+while (i <= 100 ) {
+    if (i % 20 === 0) {
+        console.log(i)
+    }
+    i++
+}
+
+// with array
+let arrX = [10, 30, 50, 70, 90, 20, 40, 60, 80, 100]
+let g = 0
+while (g < arrX.length) {
+    if (g < 5) {
+        console.log(arrX[g])
+    } else {
+        console.log(arrX[g])
+    }  
+    g++
+}
+
+//4
+
+let k = 500
+
+while (k <= 800 && k >= 500) {
+    if (k % 4 === 0) {
+        console.log(k)
+    }k++
+}
+
 
 
 
