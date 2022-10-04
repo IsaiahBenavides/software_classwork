481e9113ffeb79d388b10bc7ed6762bc9d556f9e
Benavidesisaiah@outlook.com <benavidesisaiah@outlook.com>
2022-10-04 16:47:19 -0400

    JS-Functions_HomeWork Finished

--- UNIT-1/WEEK-2/Week2_Day-2/JS-Functions_HomeWork.js
+++ UNIT-1/WEEK-2/Week2_Day-2/JS-Functions_HomeWork.js
@@ -24,17 +24,32 @@ const maxOfThree = function(x,y,z){
         return z
     }
 }
-console.log(maxOfThree(1,2,3))
-// Define a function, as a function declaration, isCharAVowelthat takes a character as an argument and returns true if it is a vowel, false otherwise.
+console.log(maxOfThree(1,5,3))
 
-// Define a function, as a function expression, sumArraythat takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]);would return 11.
+// Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
 
-// Define a function, as a function declaration, multiplyArraythat takes an array of numbers and returns the product of those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
+const vowels = ["a", "e", "i", "o", "u"]
 
-// Define a function, as a function expression, numArgsthat returns the number of arguments passed to the function when called.
+function isCharAVowel(letter){
+    letter = letter.toLowerCase();
+    if (vowels.includes(letter)){
+        return true
+    }else {
+        return false
+    }
+}
 
-// Define a function, as a function declaration, reverseStringthat takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
+console.log(isCharAVowel("E"))
+console.log(isCharAVowel("b"))
 
-// Define a function, as a function expression, longestStringInArraythat takes an array of strings as an argument and returns the length of the longest string.
+// Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]);would return 11.
 
-// Define a function, as a function declaration, stringsLongerThanthat takes an array of strings and a number as arguments, and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);would return ["hello", "morning"].
+// Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product of those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
+
+// Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
+
+// Define a function, as a function declaration, reverseString that takes a string, reverses the characters, and returns it. For example, reverseString('rockstar');would return the string "ratskcor".
+
+// Define a function, as a function expression, longestStringInArray that takes an array of strings as an argument and returns the length of the longest string.
+
+// Define a function, as a function declaration, stringsLongerThan that takes an array of strings and a number as arguments, and returns an array of the strings that are longer than the number passed in. For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3);would return ["hello", "morning"].
