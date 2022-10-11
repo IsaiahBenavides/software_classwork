46b0a883f82095d3fde0675d8f9d04e7f99f1895
Benavidesisaiah@outlook.com <benavidesisaiah@outlook.com>
2022-10-11 18:59:54 -0400

    Finished "Chef Make Dinners" Question.

--- UNIT_1/WEEK_3/Week_3-Day_2/2022-10-11_Creating-Classes-&-Factories_HW.js
+++ UNIT_1/WEEK_3/Week_3-Day_2/2022-10-11_Creating-Classes-&-Factories_HW.js
@@ -114,26 +114,33 @@ larry.ageUp();
 larry.buyHamster(squeaks);
 console.log(larry);
 
+// ==== CREATE A STORY WITH YOU PERSON CLASS ====
+
+//Instantiate a new Person named Timmy
 const timmy = new Person(`Timmy`)
 
+//Age Timmy five years
 timmy.ageUp();
 timmy.ageUp();
 timmy.ageUp();
 timmy.ageUp();
 timmy.ageUp();
 
+//At this point Timmy's a little bummed. As a precocious child, he feels he's "seen it all" already. Have him eat five times.
 timmy.eat();
 timmy.eat();
 timmy.eat();
 timmy.eat();
 timmy.eat();
 
+//Now Timmy's a little heavier than he wants to be. Kindergarten's coming up and he wants to look good. Have him exercise five times
 timmy.exercise();
 timmy.exercise();
 timmy.exercise();
 timmy.exercise();
 timmy.exercise();
 
+//Age Timmy 9 years
 timmy.ageUp();
 timmy.ageUp();
 timmy.ageUp();
@@ -144,10 +151,18 @@ timmy.ageUp();
 timmy.ageUp();
 timmy.ageUp();
 
-const gus = new Hamster(`Timmy`, `Gus`, 200);
+//Create a hamster named "Gus"
+const gus = new Hamster(`Pet Store`, `Gus`, 200);
+console.log(gus)
 
+//Set Gus's owner to the string "Timmy"
+gus.owner = `Timmy`
+ console.log(gus)
+
+//Have Timmy "buy" Gus
 timmy.buyHamster(gus);
 
+//Age Timmy 15 years
 timmy.ageUp();
 timmy.ageUp();
 timmy.ageUp();
@@ -164,10 +179,45 @@ timmy.ageUp();
 timmy.ageUp();
 timmy.ageUp();
 
+//Have Timmy eat twice
 timmy.eat();
 timmy.eat();
 
+//Have Timmy exercise twice
 timmy.exercise();
 timmy.exercise();
 
-console.log(timmy)
+console.log(timmy);
+
+// ==== CHEF MAKE DINNERS ====
+/*
+Chef should be a factory of Dinner
+Add a constructor to dinner that sets the string properties, appetizer, entree and dessert.
+Add a method on chef that takes three arguments and returns a new Dinner based on those arguments.
+Have the Chef create 3 dinners, log the dinners
+*/
+
+
+class Dinner {
+    constructor(appetizer, entree, dessert){
+        this.appetizer = appetizer;
+        this.entree = entree;
+        this.dessert = dessert;
+    };
+};
+
+class Chef {
+    constructor(name){
+        this.name = name;
+    };
+    cook(appetizer,entree,dessert){
+        let newDinner = new Dinner(appetizer,entree,dessert);
+        return newDinner;
+    };
+};
+
+const boyardee = new Chef(`Boyardee`);
+
+console.log(boyardee.cook(`Garlic bread`, `Spaghetti`, `Cheesecake`));
+console.log(boyardee.cook(`Tomato Basil Soup`, `Raviolii`, `Gelato`));
+console.log(boyardee.cook(`House Salad`, `Shrimp Alfredo`, `Tiramisù`));
