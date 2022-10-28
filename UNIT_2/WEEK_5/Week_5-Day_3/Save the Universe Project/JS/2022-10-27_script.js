6b8d25aa234f71d4663b4ab8d2f1245a21d7b1d8
Benavidesisaiah@outlook.com <benavidesisaiah@outlook.com>
2022-10-28 14:59:32 -0400

    Co-authored-by: Serjay Parks <SerjayP@users.noreply.github.com> Co-authored-by: Danjeirys <Danjeirys@users.noreply.github.com> Co-authored-by: mamin89 <mamin89@users.noreply.github.com> Co-authored-by: Latisha Bridges <LB3368@users.noreply.github.com>

--- UNIT_2/WEEK_5/Week_5-Day_3/Save the Universe Project/JS/2022-10-27_script.js
+++ UNIT_2/WEEK_5/Week_5-Day_3/Save the Universe Project/JS/2022-10-27_script.js
@@ -1,11 +1,15 @@
 
 //       SPACE BATTLE MINI LAB
 
-const game = {
-    start: function(){
-        
-    }
-}
+// Querey selectors
+let startBtn = document.querySelector('#start-button')
+let battleBtn = document.querySelector("#battle-button")
+let introText = document.querySelector("#intro-text")
+let combatLog = document.querySelector("#combat-log")
+
+
+
+
 
 // Building ships
 class USS_Earth {
@@ -14,16 +18,22 @@ class USS_Earth {
     static accuracy = 0.7;
     static attack(ship) {
         let randomNum = Math.random();
+        let newLog = document.createElement('p')
         if (randomNum.toFixed(2) < USS_Earth.accuracy) {
-            console.log(`Your hit chance: ${randomNum.toFixed(2)} is under your accuracy!`);
-            console.log(`It's a hit!`);
-            console.log(`${ship.shipName} HP: ${ship.hull}`);
             ship.hull -= USS_Earth.firepower;
+            newLog.innerText = "It's a hit! \n" + ship.shipName + " HP: " + ship.hull.toFixed(2)
+            combatLog.append(newLog)
+            console.log(`Your hit chance: ${randomNum.toFixed(2)} is under your accuracy!`);
+            // console.log(`It's a hit!`);
+            // console.log(`${ship.shipName} HP: ${ship.hull}`);
+            
             console.log(`${ship.shipName} HP: ${ship.hull.toFixed(2)}`);
             return ship.hull;
         } else {
-            console.log(`You missed the alien ship ${ship.shipName}...`);
-            console.log(`Your hit chance: ${randomNum.toFixed(2)} did not fall under your accuracy of 0.7...`);
+            newLog.innerText = `You missed ${ship.shipName}! Prepare for impact!!`
+            combatLog.append(newLog)
+            // console.log(`You missed the alien ship ${ship.shipName}...`);
+            // console.log(`Your hit chance: ${randomNum.toFixed(2)} did not fall under your accuracy of 0.7...`);
         };
     };
 };
@@ -43,16 +53,21 @@ class AlienShip {
     attack() {
         let randomNum = Math.random();
         if (randomNum < this.accuracy) {
-            console.log(`The hit chance ${randomNum.toFixed(2)} is under its accuracy of ${this.accuracy}!`);
-            console.log(`The alien ship ${this.shipName} gets a hit!`);
-            console.log(`Your HP: ${USS_Earth.hull}`);
             USS_Earth.hull -= this.firepower;
-            console.log(`Your HP after attack: ${USS_Earth.hull}`);
+            newLog.innerText = "The alien ship " + this.shipName + " gets a hit! \n" + "Your HP: " + USS_Earth.hull
+            combatLog.append(newLog)
+            // console.log(`The hit chance ${randomNum.toFixed(2)} is under its accuracy of ${this.accuracy}!`);
+            // console.log(`The alien ship ${this.shipName} gets a hit!`);
+            // console.log(`Your HP: ${USS_Earth.hull}`);
+            // USS_Earth.hull -= this.firepower;
+            // console.log(`Your HP after attack: ${USS_Earth.hull}`);
             return USS_Earth.hull;
         } else {
-            console.log(`The alien ship ${this.shipName} missed!`);
-            console.log(`Hit chance: ${randomNum.toFixed(2)}`);
-            console.log(`Accuracy: ${this.accuracy}`);
+            newLog.innerText = "The alien ship " + this.shipName + " missed! \n" + "Prepare your attack!" 
+            combatLog.append(newLog)
+            // console.log(`The alien ship ${this.shipName} missed!`);
+            // console.log(`Hit chance: ${randomNum.toFixed(2)}`);
+            // console.log(`Accuracy: ${this.accuracy}`);
         };
     };
 };
@@ -80,19 +95,75 @@ const alienFleet = [
 const battle = function () {
     for (let i = 0; i < alienFleet.length; i++) {
         do {
-            if(alienFleet[i].hull > 0){
+            // if(alienFleet[i].hull > 0 ){
                 USS_Earth.attack(alienFleet[i]);
-                if (alienFleet[i].hull > 1) {
+                if (alienFleet[i].hull >= 0) {
                     alienFleet[i].attack();
-                } else if (USS_Earth.hull > 0) {
-                    console.log(`You have defeated the alien ship! Continue?`);
-                    return;
+                    if (USS_Earth.hull >= 0) {
+                        USS_Earth.attack(alienFleet[i])
+                    } else{
+                        newLog.innerText = "You are defeated... Retry?"
+                        combatLog.append(newLog)
+                        // console.log(`You are defeted... Retry?`);
+                    }
                 } else {
-                    console.log(`You are defeted... Retry?`);
+                    newLog.innerText = "You win! The " + alienFleet[i].shipName + "is defeated!"
+                    combatLog.append(newLog)
+                    console.log(`You win! The ${alienFleet[i].shipName} is defeated.`);
                 };
-            };
-        } while (alienFleet[i].hull > 1);
+            // } ;
+        } while (alienFleet[i].hull > 0 && USS_Earth.hull > 0)
+        // prompt(`You have defeated the alien ship! Continue?`)
+        // setTimeout(prompt(`You have defeated the alien ship! Continue?`), 5000)
+        if(prompt(`You have defeated the alien ship! Continue?`) === null){
+            console.log("You didn't want to continue, now Earth will be overtaken by aliens!")
+            return
+        };
     };
 };
 
-battle()
+
+
+
+// START BUTTON 
+
+    
+startBtn.addEventListener('click', ()=>{
+    startBtn.classList.add("hide")
+    startBtn.classList.remove("startDecal")
+
+    battleBtn.classList.remove("hide")
+    battleBtn.classList.add("battleDecal")
+
+    introText.classList.add("hide")
+    combatLog.classList.remove("hide")
+    
+})
+
+battleBtn.addEventListener('click', ()=>{
+    battle()
+})
+
+// ====== REFERNCE=====
+// const button = document.querySelector('input');
+// const paragraph = document.querySelector('p');
+
+// button.addEventListener('click', updateButton);
+
+// function updateButton() {
+//   if (button.value === 'Start machine') {
+//     button.value = 'Stop machine';
+//     paragraph.textContent = 'The machine has started!';
+//   } else {
+//     button.value = 'Start machine';
+//     paragraph.textContent = 'The machine is stopped.';
+//   }
+// }
+
+
+
+
+
+
+
+console.log("I started")
