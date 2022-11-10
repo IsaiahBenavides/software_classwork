bf944127cc71868cc1211b8b26d3d5bce0c247ee
Benavidesisaiah@outlook.com <benavidesisaiah@outlook.com>
2022-11-10 15:56:26 -0500

    End of week 7

--- UNIT_2/WEEK_7/Week_7-Day_2/2022-11-09_Intro_to_Node/script.js
+++ UNIT_2/WEEK_7/Week_7-Day_2/2022-11-09_Intro_to_Node/script.js
@@ -13,10 +13,39 @@
 // console.log(day)
 
 // Don't specify path when module is in node_modules
-const request = require('request');
-request(
-	'http://jsonplaceholder.typicode.com/users',
-	function(err, res, body) {
-		console.log(body);
-	}
-);
+// const request = require('request');
+// request(
+// 	'http://jsonplaceholder.typicode.com/users',
+// 	function(err, res, body) {
+// 		console.log(body);
+// 	}
+// );
+
+// // Practice 1[]
+// const random = require('./utilities/random');
+// for (let i = 0; i < 10; i++) {
+//     console.log( random.randNum(100, 200) );
+// }
+
+// // Practice 2
+// const circle = require('./utilities/circle');
+// console.log( circle.area(50) );  // 7853.98...
+// console.log( circle.circumference(75) );  // 471.23...
+
+// const colors = ['red', 'green', 'blue'];
+
+// console.log('BEFORE the forEach...');
+
+// colors.forEach(function(color, idx) {
+//     console.log(`${idx + 1} - ${color}`);
+// });
+
+// console.log('AFTER the forEach...');
+
+console.log('Code before the asynchronous function call');
+
+setTimeout(function() {
+  console.log('setTimeout code')
+});
+
+console.log('Code after the asynchronous function call');
---
+++ UNIT_2/WEEK_7/Week_7-Day_2/2022-11-09_Intro_to_Node/utilities/circle.js
@@ -0,0 +1,7 @@
+module.exports.area = function(radius){
+    return Math.pow(radius, 2) * Math.PI
+}
+
+module.exports.circumference = function(radius){
+    return (radius * 2) * Math.PI
+}
