384f801752af5d4a635ccb6a733ed7771cb7caaf
Benavidesisaiah@outlook.com <benavidesisaiah@outlook.com>
2022-11-17 10:50:46 -0500

    Start of class work

--- UNIT_2/WEEK_8/Week_8-Day_1/express-fruits/server.js
+++ UNIT_2/WEEK_8/Week_8-Day_1/express-fruits/server.js
@@ -8,16 +8,20 @@ app.set(`view engine`, `jsx`);
 app.engine(`jsx`, reactViews.createEngine());
 
 app.get("/fruits", (req, res) => {
-  res.render(`Index`, {fruits: fruits})
-})
+  res.render(`Index`, {fruits: fruits});
+});
+
+app.get(`/fruits/new`, (req, res) =>{
+  res.render(`New`);
+});
 
 app.get("/fruits/:indexOfFruit", (req, res) => {
 //   res.send(fruits[req.params.indexOfFruit])
     res.render(`Show`, {
         fruit : fruits[req.params.indexOfFruit]
-    })
-})
+    });
+});
 
 app.listen(PORT, () => { 
-  console.log(`Listening on port: ${PORT}`)
-}) 
+  console.log(`Listening on port: ${PORT}`);
+});
